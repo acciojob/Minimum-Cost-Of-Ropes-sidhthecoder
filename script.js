35b6cff81ec89af417ce2c82387f824b10815d18
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-15 15:36:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function minCostToFormRope(event) {
+	event.preventDefault();
+	var firstinput=document.querySelector("input").value;
+    var arr=firstinput.split(",");
+    arr.sort(function (a, b) {
+    return a - b;
+  });
+
+var cost=0;
+while(arr.length>1){
+	var result=Number(arr[0])+Number(arr[1]);
+	arr.splice(0,2);
+	arr.push(result);
+	cost+=result;
+	arr.sort(function(a,b){
+		return a-b;
+	});
+}
+document.getElementById("result").textContent=cost;	
+}
